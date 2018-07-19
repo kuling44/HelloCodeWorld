e8a1155c1b452f47c45a7242a7af70c49fcf812d
kuling44 <kuling44@126.com>
2018-07-19 10:26:14 +0800

    2018-7-19-GT
    修代码注释，并删除日志文件

--- MyReflectionTest/Program.cs
+++ MyReflectionTest/Program.cs
@@ -63,11 +63,11 @@ namespace MyReflectionTest
 
             #endregion
 
-            //Assembly myAssembly = Assembly.Load("Ruanmou.DB.SqlServer");
-            //Type myType = myAssembly.GetType("Ruanmou.DB.SqlServer.GenericMethod");
-            //object obMy = Activator.CreateInstance(myType);
-            //MethodInfo methodMy = myType.GetMethod("Show1", new Type[] { typeof(string), typeof(int), typeof(DateTime) });
-            //methodMy.Invoke(obMy, new object[] { "123", 1, DateTime.Now });
+            //Assembly myAssembly = Assembly.Load("Ruanmou.DB.SqlServer");// 获取反射文件路径
+            //Type myType = myAssembly.GetType("Ruanmou.DB.SqlServer.GenericMethod");//获取反射对象的对象类型
+            //object obMy = Activator.CreateInstance(myType);获取对象
+            //MethodInfo methodMy = myType.GetMethod("Show1", new Type[] { typeof(string), typeof(int), typeof(DateTime) });//获取对象的方法体，以及参数
+            //methodMy.Invoke(obMy, new object[] { "123", 1, DateTime.Now });//反射调用程序方法体
 
             #region 助手发货测试
 
